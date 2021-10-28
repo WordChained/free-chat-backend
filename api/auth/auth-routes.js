6ac3df210c5857d7c622574a6fa25d1ded32623d
Tal Ben David <talbd51@gmail.com>
2021-10-28 19:33:48 +0300

    trying heroku again

--- api/auth/auth-routes.js
+++ api/auth/auth-routes.js
@@ -1,10 +1,12 @@
 const express = require('express')
-const {login, signup, logout} = require('./auth-controller')
+const { login, signup, logout } = require('./auth-controller')
 
 const router = express.Router()
 
-router.post('/login', login)
-router.post('/signup', signup)
+// router.post('/login', login)
+// router.post('/signup', signup)
+router.get('/login', login)
+router.get('/signup', signup)
 router.post('/logout', logout)
 
 module.exports = router
