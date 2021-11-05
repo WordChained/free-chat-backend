7c9fa19b8f0419feaab14cfe884dcbc3c4def5e3
Tal Ben David <talbd51@gmail.com>
2021-11-05 11:11:31 +0200

    a

--- server.js
+++ server.js
@@ -45,7 +45,6 @@ if (process.env.NODE_ENV === 'production') {
     // app.use(cors(corsOptions))
 } else {
     const corsOptions = {
-        // originWhitelist: [],
         origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://localhost:3000',
             'http://localhost:8081', 'http://127.0.0.1:3030', 'http://127.0.0.1:3000', 'http://localhost:3030',
             'http://192.168.1.17:8080/', 'http://192.168.1.22:8080',
--- services/socket-service.js
+++ services/socket-service.js
@@ -1,54 +1,70 @@
 const socketIo = require("socket.io");
 const logger = require('./logger-service')
 
+let numOfUsers = {}
 
 const socketService = (server, session) => {
     const io = socketIo(server, {
         cors: {
-            // origin: ["http://localhost:3000", 'http://127.0.0.1:3000', 'https://wordchained.github.io/free-chat', 'https://free-chat-1.herokuapp.com', "http://localhost:3030"],
-            origin: '*',
-            methods: ["GET", "POST", "DELETE", "PUT"],
+            origin: ["http://localhost:3000", 'http://127.0.0.1:3000', 'https://wordchained.github.io/free-chat', 'https://free-chat-1.herokuapp.com', "http://localhost:3030"],
+            // origin: '*', // doesnt work!
+            // methods: ["GET", "POST", "DELETE", "PUT"],
             credentials: true,
         }
     });
+    io.use((socket,next)=>{
+        session(socket.request,{},next)
+    })
     io.on('connect', (socket) => {
         console.log('New socket connected', socket.id);
+        socket.emit('online-users', () => {
+            return socket.adapter.sids.size
+        })
         socket.on('disconnect', () => {
             console.log('socket disconnected');
         })
-        // socket.on('test', response)
+        socket.on('leave room', topic => {
+            if (numOfUsers[topic] < 0) numOfUsers[topic] = 0
+            console.log('leave room:', topic);
+            socket.leave(topic)
+            if (numOfUsers[topic]) {
+                numOfUsers[topic]--
+                console.log('users-in-room(leave):', numOfUsers[topic]);
+                io.to(topic).emit('users-in-room', numOfUsers[topic])
+            }
+        })
+        socket.on('room topic refresh',topic=>{
+            socket.join(topic)
+            socket.myTopic = topic
+            io.to(topic).emit('users-in-room', numOfUsers[topic])
+        })
         socket.on('room topic', topic => {
             if (socket.myTopic === topic) return;
             if (socket.myTopic) {
-                socket.leave(socket.myTopic)
+                socket.leave(topic)
+                numOfUsers[topic]--
+                io.to(topic).emit('users-in-room', numOfUsers[topic])
+                console.log('left in add');
             }
             socket.join(topic)
             socket.myTopic = topic
+            // console.log(socket.request.res);
+            if (!numOfUsers[topic]) {
+                numOfUsers[topic] = 1
+            } else {
+                numOfUsers[topic]++
+            }
+            console.log('users-in-room(add):', numOfUsers[topic]);
+            io.to(topic).emit('users-in-room', numOfUsers[topic])
+        })
+        socket.on('check-num-of-users', (topic) => {
+            console.log('req sent to know num of users', numOfUsers[topic]);
+            io.to(topic).emit('users-in-room', numOfUsers[topic])
         })
         socket.on('room newMsg', msg => {
             logger.debug('topic:', socket.myTopic, 'msg:', msg)
             console.log(socket.rooms);
             io.to(socket.myTopic).emit('room addMsg', msg)
-        })
-        socket.on('room watch', roomId => {
-            logger.debug('room watch', roomId)
-            if (socket.roomId === roomId) return;
-            if (socket.roomId) {
-                socket.leave(socket.roomId);
-            }
-            // socket.join('watching:' + roomId)
-            socket.join(roomId)
-            socket.roomId = roomId;
-        })
-        socket.on('set-room-socket', roomId => {
-            logger.debug(`Setting socket.roomId = ${roomId}`)
-            socket.roomId = roomId
-        })
-        socket.on('unset-room-socket', () => {
-            delete socket.roomId
-        })
-        socket.on('room updated', (updatedRoom) => {
-            socket.to(updatedRoom._id).emit("room updated", updatedRoom);
         })
     })
 }
