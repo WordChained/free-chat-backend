b242d9c910949ce92e9ab38c1928046066167332
Tal Ben David <talbd51@gmail.com>
2021-11-12 16:23:26 +0200

    private chats sockets

--- services/socket-service.js
+++ services/socket-service.js
@@ -1,7 +1,17 @@
 const socketIo = require("socket.io");
-const logger = require('./logger-service')
+const logger = require('./logger-service');
 
 let numOfUsers = {}
+let privateRooms = {}
+let limitedRooms = {}
+
+const users = []
+
+const getRandomIntInclusive = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
 
 const socketService = (server, session) => {
     const io = socketIo(server, {
@@ -74,10 +84,69 @@ const socketService = (server, session) => {
             // console.log('req sent to know num of users', numOfUsers[topic].length);
             io.to(topic).emit('users-in-room', numOfUsers[topic].length)
         })
+        socket.on('join-private-room', ({ uid, topics }) => {
+            if (socket.inRoom) return
+            console.log('joining room...')
+            if (users.length <= 1) {
+                if (!users.find(user => user.uid === uid)) {
+                    users.push({ uid, topics })
+                    socket.join(uid)
+                }
+                console.log('no other users right now');
+                setTimeout(() => {
+                    return io.to(uid).emit('private-room-enter-msg', 'waiting for somone else to join!')
+                }, 2000)
+            } else {//if there are 2 or more people in the lobby
+                const potentialMatches = users.filter(user => {
+                    if (user.uid === uid) return false
+                    return user.topics.some(userTopic => {
+                        console.log('userTopic:', userTopic);
+                        return topics.includes(userTopic)
+                    })
+                })
+                console.log('potentialMatches:', potentialMatches);
+                if (!potentialMatches.length) {//creating a room
+                    if (!users.find(user => user.uid === uid)) users.push({ uid, topics })//adding to 'lobby'
+                    socket.join(uid)
+                    socket.inRoom = uid
+                    setTimeout(() => {
+                        return io.to(uid).emit('private-room-enter-msg', 'You are connected, waiting for another user...')
+                    }, 2000)
+                } else {//joining a room
+                    const randNum = getRandomIntInclusive(0, potentialMatches.length - 1)
+                    const randUser = potentialMatches[randNum]//one user from the potential list
+                    const idx = users.findIndex(user => user.uid === randUser.uid)//his index
+                    users.splice(idx, 1)//remove him from the array so no other users can join!
+                    socket.join(randUser.uid)//join the ready room
+                    socket.inRoom = randUser.uid
+                    setTimeout(() => {
+                        return io.to(socket.inRoom).emit('private-room-enter-msg', 'You are connected, say hello!')
+                    }, 2000)
+                }
+            }
+        })
+        socket.on('leave-private-room', ({ uid, topics }) => {
+            if (!socket.inRoom || users.includes(uid)) return
+            console.log('leaving room...');
+            users.push({ uid, topics })//adding to 'lobby'
+            socket.leave(socket.inRoom)
+            io.to(socket.inRoom).emit('private-room-enter-msg', uid === socket.inRoom ? 'You left the room.' : 'other user disconnected.')
+        })
+        //later i can find users by their uid and choose who i speak to!
+        // io.on("connection", socket => {
+        //     socket.on("private message", (anotherSocketId, msg) => {
+        //       socket.to(anotherSocketId).emit("private message", socket.id, msg);
+        //     });
+        //   });
         socket.on('room newMsg', msg => {
-            logger.debug('topic:', socket.myTopic, 'msg:', msg)
+            // logger.debug('topic:', socket.myTopic, 'msg:', msg)
             // console.log(socket.rooms);
             io.to(socket.myTopic).emit('room addMsg', msg)
+        })
+        socket.on('private-room-msg', msg => {
+            // logger.debug('topic:', socket.myTopic, 'msg:', msg)
+            // console.log(socket.rooms);
+            io.to(socket.inRoom).emit('private-room-add-msg', msg)
         })
     })
 }
