64021f0660d7540a77c5a8391f5864d7f5f094bc
Tal Ben David <talbd51@gmail.com>
2021-11-28 15:20:38 +0200

    trying a get req on login

--- api/auth/auth-routes.js
+++ api/auth/auth-routes.js
@@ -3,7 +3,7 @@ const { login, signup, logout } = require('./auth-controller')
 
 const router = express.Router()
 
-router.post('/login', login)
+router.get('/login', login)
 router.post('/signup', signup)
 router.post('/logout', logout)
 
