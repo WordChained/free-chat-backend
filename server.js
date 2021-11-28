edb6db2b2399b19eda89ec6caf5ad2a590950813
Tal Ben David <talbd51@gmail.com>
2021-11-28 11:15:17 +0200

    trying Shai's headers

--- server.js
+++ server.js
@@ -58,6 +58,14 @@ if (process.env.NODE_ENV === 'production') {
     app.use(cors(corsOptions))
     // app.use(cors())
 }
+var allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+app.use(allowCrossDomain);
 
 // app.use(timeout(360000))
 // function haltOnTimedout(req, res, next) {
