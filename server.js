dada7223e9b51bbc8cc07811e069caab84c2e05d
Tal Ben David <talbd51@gmail.com>
2021-11-28 11:27:11 +0200

    more trials

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ if (process.env.NODE_ENV === 'production') {
     // app.use(cors())
 }
 var allowCrossDomain = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://free-chat-frontend.herokuapp.com');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
